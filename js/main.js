22730483ad333921f3e1f631a8f66cb3023aed69
Jondar <Johnsantana87@gmail.com>
2022-09-12 13:20:08 -0400

    hero

--- js/main.js
+++ js/main.js
@@ -2,17 +2,52 @@
   "use strict";
 
   // CUSTOM ==========================
-  $(document).scroll(function () {
-    var st = $(this).scrollTop();
-    $("#header").css({
-      "background-position-y": -st / 20,
-    });
-    $("#headerc").css({
-      top: -st / 5,
-      bottom: st / 5,
-    });
-  });
-// CUSTOM END =============================
+  //  Hero fade transition
+  window.HeroFade_Object = {};
+  (function (window, $, that) {
+    // Constructor.
+    that.init = function () {
+      that.cache();
+
+      if (that.meetsRequirements) {
+        that.bindEvents();
+      }
+    };
+
+    // Cache all the things.
+    that.cache = function () {
+      that.$c = {
+        window: $(window),
+        heroSelector: $(".hero"),
+      };
+    };
+
+    // Combine all events.
+    that.bindEvents = function () {
+      that.$c.window.on("load", that.doHero);
+    };
+
+    // Do we meet the requirements?
+    that.meetsRequirements = function () {
+      return that.$c.heroSelector.length;
+    };
+
+    // Watch the user scroll position and fade the hero
+    that.doHero = function () {
+      $(window).scroll(function () {
+        // transition the fade by dividing div height & opacity
+        $(".hero").css(
+          "opacity",
+          1 - $(window).scrollTop() / $(".hero").height()
+        );
+      });
+    };
+
+    // Engage!
+    $(that.init);
+  })(window, jQuery, window.HeroFade_Object);
+
+  // CUSTOM END =============================
 
   $(document).ready(function ($) {
     "use strict";
